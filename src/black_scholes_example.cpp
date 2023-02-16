d6b57b63fb85fe853c2db5cc004621ba2a064933
Dirk Eddelbuettel <edd@debian.org>
2023-02-16 08:50:03 -0600

    Extend Black-Scholes example to also provide vega

--- src/black_scholes_example.cpp
+++ src/black_scholes_example.cpp
@@ -14,10 +14,10 @@ inline auto Phi(const T& x)
 }
 
 // Generates expression that computes Black-Scholes option price
-template <option_type cp, class Price, class Cache>
+template <option_type cp, class Price, class Vol, class Cache>
 auto black_scholes_option_price(const Price& S,
                                 double K,
-                                double sigma,
+                                const Vol& sigma,
                                 double tau,
                                 double r,
                                 Cache& cache)
@@ -49,32 +49,37 @@ auto black_scholes_option_price(const Price& S,
 //' black_scholes_example()
 // [[Rcpp::export]]
 void black_scholes_example() {
-    double K = 100.0;        
-    double sigma = 5;        
+    double K = 100.0;
     double tau = 30.0 / 365;
     double r = 1.25 / 100;   
-    ad::Var<double> S(105);  
+    ad::Var<double> sigma(5);
+    ad::Var<double> S(105);
     std::vector<ad::Var<double>> cache;
 
-    auto call_expr = ad::bind(
-            black_scholes_option_price<option_type::call>(
-                S, K, sigma, tau, r, cache));
-
+    auto call_expr = (black_scholes_option_price<option_type::call>(S, K, sigma, tau, r, cache));
+    auto size_pack_c = call_expr.bind_cache_size();
+    std::vector<double> del_buf_c(size_pack_c(0));
+    std::vector<double> veg_buf_c(size_pack_c(1));
+    call_expr.bind_cache({del_buf_c.data(), veg_buf_c.data()});
     double call_price = ad::autodiff(call_expr);
 
-    Rcpp::Rcout << call_price << std::endl;
-    Rcpp::Rcout << S.get_adj() << std::endl;
+    Rcpp::Rcout << "Call Price: " << call_price << std::endl;
+    Rcpp::Rcout << "Delta:      " << S.get_adj() << std::endl;
+    Rcpp::Rcout << "Vega:       " << sigma.get_adj() << std::endl;
 
     // reset adjoints before differentiating again
     S.reset_adj();
+    sigma.reset_adj();
     for (auto& c : cache) c.reset_adj();
 
-    auto put_expr = ad::bind(
-            black_scholes_option_price<option_type::put>(
-                S, K, sigma, tau, r, cache));
-
+    auto put_expr = (black_scholes_option_price<option_type::put>(S, K, sigma, tau, r, cache));
+    auto size_pack_p = put_expr.bind_cache_size();
+    std::vector<double> del_buf_p(size_pack_p(0));
+    std::vector<double> veg_buf_p(size_pack_p(1));
+    put_expr.bind_cache({del_buf_p.data(), veg_buf_p.data()});
     double put_price = ad::autodiff(put_expr);
 
-    Rcpp::Rcout << put_price << std::endl;
-    Rcpp::Rcout << S.get_adj() << std::endl;
+    Rcpp::Rcout << "Put Price:  " << put_price << std::endl;
+    Rcpp::Rcout << "Delta:      " << S.get_adj() << std::endl;
+    Rcpp::Rcout << "Vega:       " << sigma.get_adj() << std::endl;
 }
