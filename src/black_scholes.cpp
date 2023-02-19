25b29bdab9f388321d60d58557ffa3babb39af00
Dirk Eddelbuettel <edd@debian.org>
2023-02-19 10:34:11 -0600

    Also compute rho and theta, rely on ad::bind() for expression

--- src/black_scholes.cpp
+++ src/black_scholes.cpp
@@ -14,28 +14,24 @@ inline auto Phi(const T& x)
 }
 
 // Generates expression that computes Black-Scholes option price
-template <option_type cp, class Price, class Vol, class Cache>
+template <option_type cp, class Price, class Vol, class Rho, class Tau, class Cache>
 auto black_scholes_option_price(const Price& S,
                                 double K,
                                 const Vol& sigma,
-                                double tau,
-                                double r,
+                                const Rho& r,
+                                const Tau& tau,
                                 Cache& cache)
 {
-    cache.resize(3);
-    double PV = K * std::exp(-r * tau);
-    auto common_expr = (
-            cache[0] = ad::log(S / K),
-            cache[1] = (cache[0] + ((r + sigma * sigma / 2.) * tau)) / 
-                            (sigma * std::sqrt(tau)),
-            cache[2] = cache[1] - (sigma * std::sqrt(tau))
-    );
+    cache.resize(4);
+    auto common_expr = (cache[0] = K * ad::exp(-r * tau), 	// PV
+                        cache[1] = ad::log(S / K),
+                        cache[2] = (cache[1] + ((r + sigma * sigma / 2.) * tau)) / (sigma * ad::sqrt(tau)),
+                        cache[3] = cache[2] - (sigma * ad::sqrt(tau))
+                        );
     if constexpr (cp == option_type::call) {
-        return (common_expr,
-                Phi(cache[1]) * S - Phi(cache[2]) * PV);
+        return (common_expr, Phi(cache[2]) * S - Phi(cache[3]) * cache[0]);
     } else {
-        return (common_expr,
-                Phi(-cache[2]) * PV - Phi(-cache[1]) * S);
+        return (common_expr, Phi(-cache[3]) * cache[0] - Phi(-cache[2]) * S);
     }
 }
 
@@ -61,34 +57,30 @@ Rcpp::NumericMatrix black_scholes(double spot = 105,   		// current spot
                                   double vol = 5,               // annual vol (500% !!)
                                   double r = 1.25 / 100,        // short-term rate
                                   double tau = 30.0 / 365) {	// time remaining
-    ad::Var<double> Spot(spot);
+    ad::Var<double> Delta(spot);
     ad::Var<double> Vol(vol);
+    ad::Var<double> Rho(r);
+    ad::Var<double> Theta(tau);
     std::vector<ad::Var<double>> cache;
 
-    Rcpp::NumericMatrix M(2,3);
+    Rcpp::NumericMatrix M(2,5);
     Rcpp::rownames(M) = Rcpp::CharacterVector::create("call", "put");
-    Rcpp::colnames(M) = Rcpp::CharacterVector::create("value", "delta", "vega");
+    Rcpp::colnames(M) = Rcpp::CharacterVector::create("value", "delta", "vega", "rho", "theta");
 
-    auto call_expr = (black_scholes_option_price<option_type::call>(Spot, strike, Vol, tau, r, cache));
-    auto size_pack_c = call_expr.bind_cache_size();
-    std::vector<double> del_buf_c(size_pack_c(0));
-    std::vector<double> veg_buf_c(size_pack_c(1));
-    call_expr.bind_cache({del_buf_c.data(), veg_buf_c.data()});
+    auto call_expr = ad::bind(black_scholes_option_price<option_type::call>(Delta, strike, Vol, Rho, Theta, cache));
     double call_price = ad::autodiff(call_expr);
-    M.row(0) = Rcpp::NumericVector::create(call_price, Spot.get_adj(), Vol.get_adj());
+    M.row(0) = Rcpp::NumericVector::create(call_price, Delta.get_adj(), Vol.get_adj(), Rho.get_adj(), Theta.get_adj());
 
     // reset adjoints before differentiating again
-    Spot.reset_adj();
+    Delta.reset_adj();
     Vol.reset_adj();
+    Rho.reset_adj();
+    Theta.reset_adj();
     for (auto& c : cache) c.reset_adj();
 
-    auto put_expr = (black_scholes_option_price<option_type::put>(Spot, strike, Vol, tau, r, cache));
-    auto size_pack_p = put_expr.bind_cache_size();
-    std::vector<double> del_buf_p(size_pack_p(0));
-    std::vector<double> veg_buf_p(size_pack_p(1));
-    put_expr.bind_cache({del_buf_p.data(), veg_buf_p.data()});
+    auto put_expr = ad::bind(black_scholes_option_price<option_type::put>(Delta, strike, Vol, Rho, Theta, cache));
     double put_price = ad::autodiff(put_expr);
-    M.row(1) = Rcpp::NumericVector::create(put_price, Spot.get_adj(), Vol.get_adj());
+    M.row(1) = Rcpp::NumericVector::create(put_price, Delta.get_adj(), Vol.get_adj(), Rho.get_adj(), Theta.get_adj());
 
     return(M);
 }
