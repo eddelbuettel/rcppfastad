f75310b56c708863899ce0a2b7051f415119d65d
Dirk Eddelbuettel <edd@debian.org>
2023-02-23 20:20:33 -0600

    More compact and elegant code for regression (closes #1)

--- src/linear_regression.cpp
+++ src/linear_regression.cpp
@@ -20,33 +20,19 @@ Rcpp::List linear_regression(Eigen::Map<Eigen::VectorXd> theta_hat,
                              Eigen::Map<Eigen::VectorXd> y,
                              Eigen::Map<Eigen::MatrixXd> X) {
 
-    Eigen::MatrixXd theta_adj(2, 1);
+    const auto p = theta_hat.size();
+    Eigen::VectorXd theta_adj(p);
     theta_adj.setZero(); // Set adjoints to zeros.
 
     // Initialize variable.
-    ad::VarView<double, ad::mat> theta(theta_hat.data(), theta_adj.data(), 2, 1);
+    ad::VarView<double, ad::vec> theta(theta_hat.data(), theta_adj.data(), p);
 
     // Create expr. Use a row buffer to store data. Then we only need to manipulate data when
     // looping.
-    Eigen::MatrixXd x_row_buffer = X.row(0);
-    auto xi = ad::constant_view(x_row_buffer.data(), 1, X.cols());
-    Eigen::MatrixXd y_row_buffer = y.row(0);
-    auto yi = ad::constant_view(y_row_buffer.data(), 1, y.cols());
-    auto expr = ad::bind(ad::pow<2>(yi - ad::dot(xi, theta)));
+    auto expr = ad::bind(ad::norm(y - ad::dot(X, theta)));
 
-    // Seed
-    Eigen::MatrixXd seed(1, 1);
-    seed.setOnes(); // Usually seed is 1. DONT'T FORGET!
-
-    // Loop over each row to calulate loss.
-    double loss = 0;
-    for (int i = 0; i < X.rows(); ++i) {
-        x_row_buffer = X.row(i);
-        y_row_buffer = y.row(i);
-
-        auto f = ad::autodiff(expr, seed.array());
-        loss += f.coeff(0);
-    }
+    // Differentiate and get loss
+    auto loss = ad::autodiff(expr);
 
     Rcpp::List res = Rcpp::List::create(Rcpp::Named("loss") = loss,
                                         Rcpp::Named("gradient") = theta.get_adj());
