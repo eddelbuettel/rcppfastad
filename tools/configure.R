cea3df990bb57dbf2d9984cea470ecfcc1777290
Dirk Eddelbuettel <edd@debian.org>
2026-09-21 17:07:17 -0500

    Expand configure to add a compilation flag on 'local' builds (#11)

---
+++ tools/configure.R
@@ -0,0 +1,14 @@
+
+if (dir.exists(".git")) {
+    ## development from a .git directory can use these flags
+    xtraflags <- "-Wno-ignored-attributes"
+} else {
+    ## else build from tarball so stick with existing flags
+    xtraflags <- ""
+}
+win <- if (Sys.info()[["sysname"]] == "Windows") ".win" else ""
+infile <- file.path("src", paste0("Makevars", win, ".in"))
+outfile <- file.path("src", paste0("Makevars", win))
+lines <- readLines(infile)
+lines <- gsub("@XTRAFLAGS@", xtraflags, lines)
+writeLines(lines, outfile)
